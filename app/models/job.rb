d8a8e83541cd376ac667371ed9f56094b6874d1c
leandroows <leandro@oneworldservices.com>
2021-10-21 14:47:24 -0300

    Implementing concern to generate placeholders.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
+    include Placeholder
+
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     def self.angular
@@ -10,7 +12,7 @@ class Job < ApplicationRecord
     after_initialize :set_defaults
 
     def set_defaults
-        self.main_image ||= "https://via.placeholder.com/600x400"
-        self.thumb_image ||= "https://via.placeholder.com/350x200"
+        self.main_image ||= Placeholder.image_generator(width: '600', height: '400')
+        self.thumb_image ||= Placeholder.image_generator(width: '350', height: '200')
     end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,11 @@
 class Skill < ApplicationRecord
+    include Placeholder
+
     validates_presence_of :title, :percent_used
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.badge ||= Placeholder.image_generator(width: '250', height: '250')
+    end
 end
